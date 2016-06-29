eebb3ad8aa060fc01e372c1a244b585577c67a82
Greg Ritter <gjritter@gmail.com>
2016-06-29 14:15:51 -0700

    Add interstitial custom event class for Unity Ads

---
+++ AdNetworkSupport/Unity/UnityAdsInterstitialCustomEvent.m
@@ -0,0 +1,105 @@
+//
+//  UnityAdsInterstitialCustomEvent.h
+//  MoPubSDK
+//
+//  Copyright (c) 2016 MoPub. All rights reserved.
+//
+
+#import "UnityAdsInterstitialCustomEvent.h"
+#import "MPUnityRouter.h"
+#import "MPLogging.h"
+#import "UnityAdsInstanceMediationSettings.h"
+
+static NSString *const kMPUnityRewardedVideoGameId = @"gameId";
+static NSString *const kUnityAdsOptionPlacementIdKey = @"placementId";
+static NSString *const kUnityAdsOptionZoneIdKey = @"zoneId";
+
+@interface UnityAdsInterstitialCustomEvent () <MPUnityRouterDelegate>
+
+@property (nonatomic, copy) NSString *placementId;
+
+@end
+
+@implementation UnityAdsInterstitialCustomEvent
+
+- (void)dealloc
+{
+    [[MPUnityRouter sharedRouter] clearDelegate:self];
+}
+
+- (void)requestRewardedVideoWithCustomEventInfo:(NSDictionary *)info
+{
+    NSString *gameId = [info objectForKey:kMPUnityRewardedVideoGameId];
+    self.placementId = [info objectForKey:kUnityAdsOptionPlacementIdKey];
+    if (self.placementId == nil) {
+        self.placementId = [info objectForKey:kUnityAdsOptionZoneIdKey];
+    }
+    if (self.placementId == nil) {
+        [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:nil];
+    } else {
+        [[MPUnityRouter sharedRouter] requestVideoAdWithGameId:gameId placementId:self.placementId delegate:self];
+    }
+}
+
+- (BOOL)hasAdAvailable
+{
+    return [[MPUnityRouter sharedRouter] isAdAvailableForPlacementId:self.placementId];
+}
+
+- (void)presentRewardedVideoFromViewController:(UIViewController *)viewController
+{
+    if ([self hasAdAvailable]) {
+        [[MPUnityRouter sharedRouter] presentVideoAdFromViewController:viewController customerId:nil placementId:self.placementId settings:nil delegate:self];
+    } else {
+        MPLogInfo(@"Failed to show Unity rewarded video: Unity now claims that there is no available video ad.");
+        // TODO: MoPub doesn't have a domain for interstitial errors, returning nil for now
+        [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:nil];
+    }
+}
+
+- (void)handleCustomEventInvalidated
+{
+    [[MPUnityRouter sharedRouter] clearDelegate:self];
+}
+
+- (void)handleAdPlayedForCustomEventNetwork
+{
+    // If we no longer have an ad available, report back up to the application that this ad expired.
+    // We receive this message only when this ad has reported an ad has loaded and another ad unit
+    // has played a video for the same ad network.
+    if (![self hasAdAvailable]) {
+        [self.delegate interstitialCustomEventDidExpire:self];
+    }}
+
+#pragma mark - MPUnityRouterDelegate
+
+- (void)unityAdsReady:(NSString *)placementId
+{
+    [self.delegate interstitialCustomEvent:self didLoadAd:placementId];
+}
+
+- (void)unityAdsDidError:(UnityAdsError)error withMessage:(NSString *)message
+{
+    // TODO: MoPub doesn't have a domain for interstitial errors, returning nil for now
+    [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:nil];
+}
+
+- (void) unityAdsDidStart:(NSString *)placementId
+{
+    [self.delegate interstitialCustomEventWillAppear:self];
+    [self.delegate interstitialCustomEventDidAppear:self];
+}
+
+- (void) unityAdsDidFinish:(NSString *)placementId withFinishState:(UnityAdsFinishState)state
+{
+    [self.delegate interstitialCustomEventWillDisappear:self];
+    [self.delegate interstitialCustomEventDidDisappear:self];
+}
+
+- (void)unityAdsDidFailWithError:(NSError *)error
+{
+    // TODO: MoPub doesn't have a domain for interstitial errors, returning nil for now
+    [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:nil];
+}
+
+@end
